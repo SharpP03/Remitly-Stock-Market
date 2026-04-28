f4beb64bf53e179336c46075bac9a0b9afcae4e4
SharpP03 <Patrykstoncel186@wp.pl>
2026-04-28 20:37:32 +0200

    chore: application.properties update.

--- src/main/java/remitly/stockmarket/model/Stock.java
+++ src/main/java/remitly/stockmarket/model/Stock.java
@@ -1,16 +1,13 @@
 package remitly.stockmarket.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Entity
 @Getter
 @Setter
-
 public class Stock {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/remitly/stockmarket/model/WalletPosition.java
+++ src/main/java/remitly/stockmarket/model/WalletPosition.java
@@ -1,15 +1,6 @@
 package remitly.stockmarket.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,11 +17,11 @@ public class WalletPosition {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(optional = false)
     @JoinColumn(name = "wallet_id", nullable = false)
     private Wallet wallet;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(optional = false)
     @JoinColumn(name = "stock_id", nullable = false)
     private Stock stock;
 
